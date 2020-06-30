408798885a9fcd1e2da3415a8c39c7e24cc98d1c
Barnali <mail2barnali1990@gmail.com>
2020-06-30 12:46:30 +0530

    Added a common CRUD interface . Closes #8

--- pet-clinic-data/src/main/java/com/barnali/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/barnali/petclinic/services/PetService.java
@@ -1,15 +1,6 @@
 package com.barnali.petclinic.services;
 
 import com.barnali.petclinic.model.Pet;
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findByLastName(String lastName);
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 }
