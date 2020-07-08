b7707e380aaf2bdeb408d2df3b741fee04382a44
Barnali <mail2barnali1990@gmail.com>
2020-07-08 09:21:36 +0530

    Map based implementation of PetType service. Closes #29

---
+++ pet-clinic-data/src/main/java/com/barnali/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.barnali.petclinic.services;
+
+import com.barnali.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
---
+++ pet-clinic-data/src/main/java/com/barnali/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,36 @@
+package com.barnali.petclinic.services.map;
+
+import com.barnali.petclinic.model.PetType;
+import com.barnali.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
