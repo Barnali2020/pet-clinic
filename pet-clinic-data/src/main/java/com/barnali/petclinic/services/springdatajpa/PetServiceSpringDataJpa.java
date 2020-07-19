e232481d97004ffb76ef57c00507b1ddeb94392d
Barnali <mail2barnali1990@gmail.com>
2020-07-19 11:34:39 +0530

    Added Pet service to support Spring Data JPA. Closes #42

---
+++ pet-clinic-data/src/main/java/com/barnali/petclinic/services/springdatajpa/PetServiceSpringDataJpa.java
@@ -0,0 +1,50 @@
+package com.barnali.petclinic.services.springdatajpa;
+
+import com.barnali.petclinic.model.Pet;
+import com.barnali.petclinic.repositories.PetRepository;
+import com.barnali.petclinic.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spring-data-jpa")
+public class PetServiceSpringDataJpa implements PetService {
+
+    private PetRepository petRepository;
+
+    @Autowired
+    public PetServiceSpringDataJpa(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return petRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petRepository.deleteById(id);
+    }
+}
