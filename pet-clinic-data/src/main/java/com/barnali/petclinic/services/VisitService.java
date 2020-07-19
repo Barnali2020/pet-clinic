0edee91090f56c9b967d11de5837559f5231a6d8
Barnali <mail2barnali1990@gmail.com>
2020-07-19 12:35:45 +0530

    Added Visit service to support Map based implementation. Closes #45

---
+++ pet-clinic-data/src/main/java/com/barnali/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.barnali.petclinic.services;
+
+import com.barnali.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/barnali/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,44 @@
+package com.barnali.petclinic.services.map;
+
+import com.barnali.petclinic.model.Pet;
+import com.barnali.petclinic.model.PetType;
+import com.barnali.petclinic.model.Visit;
+import com.barnali.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(object == null || object.getPet() == null || object.getPet().getId() == null
+                || object.getPet().getOwner() == null
+                || object.getPet().getOwner().getId() == null){
+                throw new RuntimeException("Invalid visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
